9c13c7c1393b39be6c2163d698047fe7f0b8f482
Zoe Pointon <zoepointon123@gmail.com>
2018-05-02 23:11:23 +0100

    betterness

--- js/script-all.js
+++ js/script-all.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}

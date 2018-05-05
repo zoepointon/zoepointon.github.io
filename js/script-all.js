a5d7d7c1c3b70e9535ba1b2cc7810660b099a677
Zoe Pointon <zoepointon123@gmail.com>
2018-05-05 12:23:10 +0100

    responsive

--- js/script-all.js
+++ js/script-all.js
@@ -5,3 +5,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+// on load finished
+$(window).load(function() {
+    // select element and fade it out
+    $('.loading').fadeOut();
+});

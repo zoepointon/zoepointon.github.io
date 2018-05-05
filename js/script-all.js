e68a982f4c308fa747a59daed06d693adf5a74fd
Zoe Pointon <zoepointon123@gmail.com>
2018-05-05 12:53:34 +0100

    re order

--- js/script-all.js
+++ js/script-all.js
@@ -1,3 +1,9 @@
+// on load finished
+$(window).load(function() {
+    // select element and fade it out
+    $('.loading').fadeOut();
+});
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
@@ -5,9 +11,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
-
-// on load finished
-$(window).load(function() {
-    // select element and fade it out
-    $('.loading').fadeOut();
-});

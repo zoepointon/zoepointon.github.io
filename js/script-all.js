66a9df48e59ae68cad321bd98512b3f51c12a4c2
Zoe Pointon <zoepointon123@gmail.com>
2019-08-30 23:50:52 +0100

    Portfolio Update
    New project pages. Minor updates to index and about.

--- js/script-all.js
+++ js/script-all.js
@@ -1,3 +1,7 @@
+$(function(){
+  $("#nav-placeholder").load("nav.html");
+});
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
@@ -5,3 +9,29 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+
+$(document).ready(function() {
+  $(".animsition-overlay").animsition({
+    inClass: 'overlay-slide-in-left',
+    outClass: 'overlay-slide-out-left',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : true,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
